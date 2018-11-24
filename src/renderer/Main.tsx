97a138c980a0257839aca7b5663ef949ed6f8845
Leo Young <420760135@qq.com>
2018-11-24 23:57:11 +0800

    feat: add monaco-editor

--- src/renderer/Main.tsx
+++ src/renderer/Main.tsx
@@ -1,9 +1,28 @@
+import * as monaco from 'monaco-editor';
 import React from 'react';
-
 class Main extends React.Component {
+  private editorRef = React.createRef<HTMLDivElement>();
+
+  componentDidMount() {
+    if (this.editorRef && this.editorRef.current) {
+      monaco.editor.create(this.editorRef.current, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+          '\n'
+        ),
+        language: 'javascript'
+      });
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div
+        ref={this.editorRef}
+        style={{
+          width: '500px',
+          height: '500px'
+        }}
+      >
         L-Slides
       </div>
     );
