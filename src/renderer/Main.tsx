17781d73df144fb87158021af52a2b93d8fa0e73
Leo Young <420760135@qq.com>
2018-12-02 20:52:30 +0800

    feat: add showdown

--- src/renderer/Main.tsx
+++ src/renderer/Main.tsx
@@ -2,6 +2,7 @@ import CodeMirror from 'codemirror/lib/codemirror';
 import 'codemirror/mode/gfm/gfm';
 import 'codemirror/mode/markdown/markdown';
 import React from 'react';
+import showdown from 'showdown';
 
 declare global {
   // tslint:disable-next-line:interface-name
@@ -10,13 +11,23 @@ declare global {
 
 class Main extends React.Component {
   private editorRef = React.createRef<HTMLDivElement>();
+  private previewRef = React.createRef<HTMLDivElement>();
 
   private editor: CodeMirror.Editor | null = null;
 
+  private converter: showdown.Converter | null = null;
+
+  constructor() {
+    super({});
+    this.converter = new showdown.Converter();
+  }
+
   componentDidMount() {
+    const initValue = '# Hello, world!';
+    this.updatePreview(initValue);
     if (this.editorRef.current) {
       this.editor = CodeMirror(this.editorRef.current, {
-        value: '# Hello, world!',
+        value: initValue,
         mode: {
           name: 'gfm',
           highlightFormatting: true
@@ -26,8 +37,7 @@ class Main extends React.Component {
       });
       this.editor.on('change', (editor, change) => {
         const value = editor.getValue();
-        // tslint:disable-next-line:no-console
-        console.log(value, change);
+        this.updatePreview(value);
       });
     }
   }
@@ -35,14 +45,37 @@ class Main extends React.Component {
   render() {
     return (
       <div
-        ref={this.editorRef}
-        id="container"
         style={{
           width: '100%',
-          height: '100%'
+          height: '100%',
+          display: 'flex',
+          flexWrap: 'nowrap',
+          justifyContent: 'space-around'
         }}
-      />
+      >
+        <div
+          ref={this.editorRef}
+          id="container"
+          style={{
+            width: '50%',
+            height: '100%'
+          }}
+        />
+        <div
+          ref={this.previewRef}
+          style={{
+            width: '50%',
+            height: '100%'
+          }}
+        />
+      </div>
     );
+  }
+
+  private updatePreview(value: string) {
+    if (this.previewRef.current && this.converter) {
+      this.previewRef.current.innerHTML = this.converter.makeHtml(value);
+    }
   }
 }
 
